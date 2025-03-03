1759d1a03ce7888f5203bd1532539eb1860e8735
Pranjal Rai <raipranjalofficial@gmail.com>
2025-03-03 20:04:18 +0530

    progressed on sell component

--- src/components/SellComponent.js
+++ src/components/SellComponent.js
@@ -1,40 +1,41 @@
+import { useState } from "react"
 import BlackButton from "./buttons/BlackButton"
 import DropdownButton from "./inputs/DropDownButton"
 import Input from "./inputs/Input"
+import RadioButtons from "./inputs/RadioButtons"
 
 export default function SellComponent(){
-    let pro_catg = [
-        "Grocery",
-        "Utensil", "Toys", "Dairy", "Furniture"
-       ]
-    return <div className="right-section md:w-[75%] h-screen bg-primary p-10">
-    <div className="text-3xl">Create an Order</div>
-    <div className="bg-white mt-10  px-10">
-    <div className ="Product-details flex justify-between">
-    <div>
-    <DropdownButton placeholder="Product Category" id="product-category" options={pro_catg} />
-   
-  </div>
+const [userTypeNew, setUserTypeNew] = useState(true)
+let pro_catg = [
+    "Grocery",
+    "Utensil", "Toys", "Dairy", "Furniture"
+    ]
+    return (
+<div className="right-section md:w-[75%] h-screen bg-primary p-10">
+<div className="text-3xl">Create an Order</div>
+<div className="bg-white mt-10  p-10">
+<div className ="Product-details flex justify-between">
+<div>
+<DropdownButton placeholder="Product Category" id="product-category" options={pro_catg} />
+</div>
 <div>
 <Input type="number" className="border-2 border-gray" placeholder="Product Name"/>
 </div>
 </div>
-<div className ="Product-Quantity flex flex-wrap justify-between"><div>
-<input type="radio" id="stock-option" name="quantity_type" value="stock" />
-  <label for="stock">Stock</label>
-  <input type="radio" id="unit-option" name="quantity_type" value="unit" />
-  <label for="unit">Unit</label>
+<div className ="Product-Quantity flex flex-wrap justify-between my-10 ">
+<div className="mr-1">
+<RadioButtons name="Quantity Type" values={["Stock", "Unit"]} />
 </div>
 <div><Input placeholder="Enter the Quantity" value="" onChange=""/></div>
 </div>
 <div className ="Customer-details/Total-Amount flex flex-wrap justify-between">
 <div className="customer-details">
-<div className=""><span>Existing Customer</span><span>Mew Customer</span></div>
-<div><Input type="text" placeholder="Enter Customer Id" /></div>
-<div><BlackButton name="Create an Order" /></div>
+<div className="mb-5"><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-transparent`: `bg-secondary text-white`}`} onClick={()=>setUserTypeNew(false)}>Existing Customer</span><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-secondary text-white`:`bg-transparent`}`} onClick={()=>setUserTypeNew(true)}>New Customer</span></div>
+<div className ="my-5"><Input type="text" placeholder="Enter Customer Id" /></div>
+<div className=""><BlackButton name="Create an Order" /></div>
 </div>
 <div className="total-amount"></div>
 </div>
 </div>
 </div>
-}
+    )}
--- src/components/buttons/BlackButton.js
+++ src/components/buttons/BlackButton.js
@@ -2,7 +2,7 @@ export default function BlackButton(props) {
   return (
     <button
       onClick={props.onClick}
-      className="bg-secondary text-white px-4 py-2 rounded w-full h-full"
+      className="bg-secondary text-white px-4 py-2 w-full h-full"
     >
       {props.name}
     </button>
