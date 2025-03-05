d4b5f9564ea73f35c05fb95d9b7ae445c45defe5
Pranjal Rai <raipranjalofficial@gmail.com>
2025-03-05 21:48:09 +0530

    --added: functionality in form- sellcomponent

--- src/components/SellComponent.js
+++ src/components/SellComponent.js
@@ -6,12 +6,13 @@ import RadioButtons from "./inputs/RadioButtons"
 
 export default function SellComponent(){
 const [userTypeNew, setUserTypeNew] = useState(true)
-const [orderDetails, order_Details] = useState({
+const [showEmail, setShowEmail] = useState(false)
+const [orderDetails, setOrderDetails] = useState({
     product_category:"",
     product_name:"",
     quantity_type_stock: true,
-    product_quantity :userTypeNew,
-    customer_type_new: true,
+    product_quantity : 0,
+    customer_type_new: userTypeNew,
     customer_id: "",
     customer_name:"",
     customer_address:"",
@@ -41,16 +42,59 @@ let pro_catg = [
 </div>
 <div className ="Customer-details/Total-Amount flex flex-wrap justify-between">
 <div className="customer-details">
-<div className="mb-5"><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-transparent`: `bg-secondary text-white`}`} onClick={()=>setUserTypeNew(false)}>Existing Customer</span><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-secondary text-white`:`bg-transparent`}`} onClick={()=>setUserTypeNew(true)}>New Customer</span></div>
+<div className="mb-5"><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-transparent`: `bg-secondary text-white`}`} onClick={()=>{setUserTypeNew(false)
+    setShowEmail(false)
+}}>Existing Customer</span><span className={`border-2 border-gray p-2 ${userTypeNew? `bg-secondary text-white`:`bg-transparent`}`} onClick={()=>{setUserTypeNew(true)
+    setShowEmail(false)
+}}>New Customer</span></div>
 {!userTypeNew?<div className ="my-5">
-<Input type="text" placeholder="Enter Customer Id" value={orderDetails['customer_id']}/></div>:
-<div className ="my-5"><Input type="text" placeholder="Enter Customer Name" onChange={()=>{
-    setUserTypeNew(prevState=>({
+<Input type="text" placeholder="Enter Customer Id" value={orderDetails['customer_id']} onChange={(e)=>{
+    setOrderDetails(prevState =>({
+        ...prevState,
+       customer_id: e.target.value
+    }))
+}}/></div>:
+<div className ="my-5">
+  {showEmail?<><Input type="text" placeholder="Enter Customer's phone number" onChange={(e)=>{
+    setOrderDetails(prevState=>({
+        ...prevState,
+        customer_contact: e.target.value
+    }))
+}} value={orderDetails['customer_contact']} />
+<Input type="text" placeholder="Enter Customer's Email'" className="mt-5" onChange={(e)=>{
+    setOrderDetails(prevState=>({
+        ...prevState,
+        customer_email: e.target.value
+    }))
+}} value={orderDetails['customer_email']} /></>:<>  <Input type="text" placeholder="Enter Customer Name" onChange={(e)=>{
+    setOrderDetails(prevState=>({
         ...prevState,
         customer_name: e.target.value
     }))
-}} value={orderDetails['customer_name']} /> <Input type="text" placeholder="Enter Shipping Address" className="mt-5" value={orderDetails['customer_address']}/></div>}
-<div className=""><BlackButton name={`${userTypeNew &&(orderDetails['customer_name']!="" && orderDetails["customer_address"]!="") ? "Next":"Create an Order"}`}/></div>
+}} value={orderDetails['customer_name']} />
+ <Input type="text" placeholder="Enter Shipping Address" className="mt-5" onChange={(e)=>{
+    setOrderDetails(prevState =>({
+        ...prevState,
+        customer_address: e.target.value
+    }))
+}}
+value={orderDetails['customer_address']}
+/></>}
+</div>}
+<div className="">
+    <BlackButton onClick={!userTypeNew? ()=>{}:()=>{
+    if (orderDetails['customer_name'].length>3 &&orderDetails['customer_address'].length>10){
+        setShowEmail(true)
+    }
+    else if (orderDetails['customer_name'].length<=3){
+        alert("Customer Name too small")
+    }
+    else{
+        alert("Customer's address too small")
+    }
+}
+}  name={`${!showEmail && userTypeNew ? "Next":"Create an Order"}`}/>
+</div>
 </div>
 <div className="total-amount"></div>
 </div>
