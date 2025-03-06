e18a644c75242354550d2a74ccbc3d176829bdb9
Pranjal Rai <raipranjalofficial@gmail.com>
2025-03-06 21:31:09 +0530

    --added: css for button

--- src/components/buttons/BlackButton.js
+++ src/components/buttons/BlackButton.js
@@ -2,7 +2,7 @@ export default function BlackButton({name, onClick, className}) {
   return (
     <button
       onClick={onClick}
-      className={`bg-secondary text-white px-4 py-2 w-full h-full ${className}`}
+      className={`bg-secondary text-white px-4 py-2 ${className}`}
     >
       {name}
     </button>
