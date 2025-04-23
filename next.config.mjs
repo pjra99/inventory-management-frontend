851dc0a24f3ac21eddc779a01e05d6c4540caea4
Pranjal Rai <62153142+pjra99@users.noreply.github.com>
2025-04-23 13:54:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { withNetlify } from '@netlify/next';
-
-const nextConfig = {
-  // Add any Next.js config options you need here
-};
-
-export default withNetlify(nextConfig);
+ const nextConfig = {};
+ 
+ export default nextConfig;
