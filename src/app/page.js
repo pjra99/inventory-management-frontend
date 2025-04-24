926b69fbfe04c7ce92259391c9c6fe45287e8216
Pranjal Rai <62153142+pjra99@users.noreply.github.com>
2025-04-24 11:46:54 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,23 @@
 "use client";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 import { base_url } from "@/API";
 import { apiCall } from "@/utils/apiCall";
-import { redirect } from "next/navigation";
-import { useSelector } from "react-redux";
+
 export default function Home() {
-  const signedIn = useSelector((state)=>state.change.signedIn)
-  apiCall('','GET', base_url, '')
-  const org_id = useSelector(state=>state.change.org_id)
-  org_id?redirect('/home'):redirect("/signin");
+  const signedIn = useSelector((state) => state.change.signedIn);
+  const org_id = useSelector((state) => state.change.org_id);
+  const router = useRouter();
+
+  useEffect(() => {
+    apiCall('', 'GET', base_url, '');
+    if (org_id) {
+      router.push("/home");
+    } else {
+      router.push("/signin");
+    }
+  }, [org_id]);
+
+  return null; // You can also return a loader if you want
 }
