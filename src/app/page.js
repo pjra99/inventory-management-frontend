5704df25067a33ec67c34d6b37f408bb1c69bda2
Pranjal Rai <62153142+pjra99@users.noreply.github.com>
2025-04-24 11:00:22 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,6 @@ import { useSelector } from "react-redux";
 export default function Home() {
   const signedIn = useSelector((state)=>state.change.signedIn)
   apiCall('','GET', base_url, '')
-  const org_id = localStorage.getItem("org_id")
+  const org_id = useSelector(state=>state.change.org_id)
   org_id?redirect('/home'):redirect("/signin");
 }
